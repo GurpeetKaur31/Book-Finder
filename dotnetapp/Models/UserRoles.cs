37814526ef9c4a6441941b422779d73e0b3517ce
Gurpreet Kaur Jassal <97333714+GurpeetKaur31@users.noreply.github.com>
2026-02-12 10:46:17 +0530

    Create UserRoles.cs

---
+++ dotnetapp/Models/UserRoles.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnetapp.Models
+{
+    public static class UserRoles
+    {
+        public const string BookRecommender = "BookRecommender";
+        public const string BookReader = "BookReader";
+
+    }
+}
